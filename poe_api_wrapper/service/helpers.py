edc1a329692063a49c1e73a345eae1c3957ca222
xzwthu <xzw14@tsinghua.org.cn>
2026-03-10 12:10:11 +0800

    Revert "```"
    This reverts commit 27e9b5482877f17d7bdf7ac572bc42a023c51de9.

--- poe_api_wrapper/service/helpers.py
+++ poe_api_wrapper/service/helpers.py
@@ -29,14 +29,10 @@ def _ensure_nltk_resources_sync() -> None:
             resource_find("corpora/stopwords")
         except LookupError:
             missing.append("stopwords")
-        # New NLTK versions renamed 'punkt' to 'punkt_tab'; check both.
         try:
-            resource_find("tokenizers/punkt_tab")
+            resource_find("tokenizers/punkt")
         except LookupError:
-            try:
-                resource_find("tokenizers/punkt")
-            except LookupError:
-                missing.append("punkt_tab")
+            missing.append("punkt")
         for package in missing:
             logger.warning("NLTK {} not found. Downloading...", package)
             nltk_download(package, quiet=True)
